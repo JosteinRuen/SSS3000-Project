99db86431b00d9a09a6e3f9427fe628e3bdebcb2
JosteinRuen <josteinruen@gmail.com>
2023-05-25 11:02:32 +0200

    recognition of recipies

---
+++ Project folder/create_recipies.py
@@ -0,0 +1,14 @@
+
+def initialize_recipies():
+    recipies = []
+
+    recipies.append({"name": "Fruit salad", "ingredients": ["Apple", "Kiwi", "Strawberry"]})
+    recipies.append({"name": "Apple Smoothie", "ingredients": ["Apple"]})
+    recipies.append({"name": "Strawberry, Apple, & Banana Smoothie", "ingredients": ["Strawberry", "Apple", "Banana"], "URL": "https://www.bigoven.com/recipe/strawberry-apple-banana-smoothie/1464590"})
+    recipies.append({"name": "Pomegranate Pizazz", "ingredients": ["Cucumber", "Orange", "Apple", "Pomegranate", "Lemon"], "URL": "https://juicerecipes.com/recipes/pomegranate-pizazz-94"})
+    recipies.append({"name": "Mango-Pineapple Sorbet", "ingredients": ["Banana", "Mango", "Pineapple", "Lemon"], "URL": "https://www.womansday.com/food-recipes/food-drinks/recipes/a10187/mango-pineapple-sorbet-121787/"})
+    recipies.append({"name": "Fruit Kebabs", "ingredients": [ "Strawberry", "Cherry", "Kiwi"], "URL": "https://www.chabad.org/recipes/recipe_cdo/aid/2832751/jewish/Fruit-Kebabs.htm"})
+    recipies.append({"name": "Tomato Cucumber Salad", "ingredients": ["Tomato", "Cucumber", "Onion", "Lemon"], "URL": "https://www.allrecipes.com/recipe/14157/tomato-cucumber-salad/"})
+    recipies.append({"name": "Baked Potato 'n Turnip Fries", "ingredients": ["Turnip", "Potato"], "URL": "https://www.hungry-girl.com/recipes/baked-potato-n-turnip-fries"})
+    recipies.append({"name": "Oven-roast potatoes", "ingredients": ["Garlic", "Potato"], "URL": "https://www.goodhousekeeping.com/uk/food/recipes/a538065/oven-roast-potatoes/"})
+    return recipies
