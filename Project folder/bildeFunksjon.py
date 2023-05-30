ff64ab21e009d98f8927cdb8471af61f21de089d
Qasim <98107904+Sato2023@users.noreply.github.com>
2023-05-30 11:32:31 +0200

    Add files via upload

--- Project folder/bildeFunksjon.py
+++ Project folder/bildeFunksjon.py
@@ -5,15 +5,21 @@ bilde_mappe = "mappe "
 
 bilde = cv2.VideoCapture(0)
 
-bilde.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-bilde.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)
+while(True):
+    bilde.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
+    bilde.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)
 
-ret, frame = bilde.read()
+    ret, frame = bilde.read()
 
-frame = cv2.rotate(frame, cv2.ROTATE_180)
+    frame = cv2.rotate(frame, cv2.ROTATE_180)
 
-bilde_sted = os.path.join(bilde_mappe, 'image.jgp')
+    bilde_sted = os.path.join(bilde_mappe, 'image.jgp')
 
-cv2.imwrite(bilde_sted, frame)
+    cv2.imwrite(bilde_sted, frame)
+
+    keyen = cv2.waitKey(1)
+    if keyen == ord('q'): 
+        break
 
 bilde.release()
+cv2.destroyAllWindows()
